28896515b59a94d7670a4f9ea9801adf7134b7cb
Gustavo <gubp81@gmail.com>
2015-03-30 22:56:21 -0400

    offer commit

---
+++ OnlineRealEstateNew/src/com/main/beans/OfferBean.java
@@ -0,0 +1,268 @@
+/**
+ * 
+ */
+package com.main.beans;
+
+import java.util.Date;
+
+
+/**
+ * @author test
+ *
+ */
+public class OfferBean {
+	private int propertyid;
+	private String address;
+	private byte[] image;
+	private Date postDate;
+	private int offers;
+	private String type;
+	private int size;
+	private String price;
+	private String region;
+	private boolean school;
+	private boolean hospital;
+	private boolean metro;
+	private boolean shopping_mall;
+	private String schoolValue;
+	private String metroValue;
+	private String hospitalValue;
+	private String shoppingMallValue;
+	private String description;
+	private String encodedImage;
+
+	/**
+	 * @return the propertyid
+	 */
+	public int getpropertyid() {
+		return propertyid;
+	}
+
+	/**
+	 * @return the propertyid
+	 */
+	public void setpropertyid(int propertyid) {
+		this.propertyid = propertyid;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	//	private String nearby;
+	/**
+	 * @return the nearby
+	 */
+	//	public String getNearby() {
+	//		return nearby;
+	//	}
+	//	/**
+	//	 * @param nearby the nearby to set
+	//	 */
+	//	public void setNearby(String nearby) {
+	//		this.nearby = nearby;
+	//	}
+	/**
+	 * @return the price
+	 */
+	public String getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	/**
+	 * @return the size
+	 */
+	public int getSize() {
+		return size;
+	}
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public String getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getRegion() {
+
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public boolean getSchool() {
+
+		return school;
+	}
+	public void setSchool(boolean school) {
+		this.school = school;
+	}
+	public boolean isHospital() {
+
+		return hospital;
+	}
+	public void setHospital(boolean hospital) {
+		this.hospital = hospital;
+	}
+	/**
+	 * @return the metro
+	 */
+	public boolean isMetro() {
+		return metro;
+	}
+	/**
+	 * @param metro the metro to set
+	 */
+	public void setMetro(boolean metro) {
+		this.metro = metro;
+	}
+	/**
+	 * @return the shopping_mall
+	 */
+	public boolean isShopping_mall() {
+		return shopping_mall;
+	}
+	/**
+	 * @param shopping_mall the shopping_mall to set
+	 */
+	public void setShopping_mall(boolean shopping_mall) {
+		this.shopping_mall = shopping_mall;
+
+	}
+	/**
+	 * @return the schoolValue
+	 */
+	public String getSchoolValue() {
+		return schoolValue;
+	}
+	/**
+	 * @param schoolValue the schoolValue to set
+	 */
+	public void setSchoolValue(String schoolValue) {
+		this.schoolValue = schoolValue;
+	}
+	/**
+	 * @return the metroValue
+	 */
+	public String getMetroValue() {
+		return metroValue;
+	}
+	/**
+	 * @param metroValue the metroValue to set
+	 */
+	public void setMetroValue(String metroValue) {
+		this.metroValue = metroValue;
+	}
+	/**
+	 * @return the hospitalValue
+	 */
+	public String getHospitalValue() {
+		return hospitalValue;
+	}
+	/**
+	 * @param hospitalValue the hospitalValue to set
+	 */
+	public void setHospitalValue(String hospitalValue) {
+		this.hospitalValue = hospitalValue;
+	}
+	/**
+	 * @return the shoppingMallValue
+	 */
+	public String getShoppingMallValue() {
+		return shoppingMallValue;
+	}
+	/**
+	 * @param shoppingMallValue the shoppingMallValue to set
+	 */
+	public void setShoppingMallValue(String shoppingMallValue) {
+		this.shoppingMallValue = shoppingMallValue;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the image
+	 */
+	/**
+	 * @return the image
+	 */
+	public byte[] getImage() {
+		return image;
+	}
+	/**
+	 * @param inputStream the image to set
+	 */
+	public void setImage(byte[] bs) {
+		this.image = bs;
+	}
+	/**
+	 * @return the encodedImage
+	 */
+	public String getEncodedImage() {
+		return encodedImage;
+	}
+	/**
+	 * @param encodedImage the encodedImage to set
+	 */
+	public void setEncodedImage(String encodedImage) {
+		this.encodedImage = encodedImage;
+	}
+	/**
+	 * @return the postDate
+	 */
+	public Date getPostDate() {
+		return postDate;
+	}
+	/**
+	 * @param postDate the postDate to set
+	 */
+	public void setPostDate(Date postDate) {
+		this.postDate = postDate;
+	}
+	/**
+	 * @return the offers
+	 */
+	public int getOffers() {
+		return offers;
+	}
+	/**
+	 * @param offers the offers to set
+	 */
+	public void setOffers(int offers) {
+		this.offers = offers;
+	}
+
+
+}
+
+
+
+
